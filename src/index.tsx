d7339a7076eb75bfa32299edc75be12a0be1d884
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-12-25 13:07:12 +0530

    reducing code duplication around fetch

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
 
--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,50 +10,59 @@ export const fetchPlugin = (userInput: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, async (args: any) => {
+        return {
+          loader: 'jsx',
+          contents: userInput,
+        };
+      });
+
+      // check to see if we have already fetched the file
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        // our index.js
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: userInput,
-          };
-        } else {
-          // fetch data from unkpg
+        const cachedRes = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        // return immediately
+        if (cachedRes) {
+          return cachedRes;
+        }
+      });
 
-          // check to see if we have already fetched the file
-          const cachedRes = await fileCache.getItem<esbuild.OnLoadResult>(
-            args.path
-          );
-          // return immediately
-          if (cachedRes) {
-            return cachedRes;
-          }
-          // else fetch and store response in cache
-          const { data, request } = await axios.get(args.path);
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
 
-          const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
 
-          const escaped = data
-            .replace(/\n/g, '')
-            .replace(/"/g, '\\"')
-            .replace(/'/g, "\\'");
-          const contents =
-            fileType === 'css'
-              ? `
+        const contents = `
             const style = document.createElement('style');
             style.innerText = '${escaped}';
             document.head.appendChild(style);
-          `
-              : data;
-          const res: esbuild.OnLoadResult = {
-            loader: 'jsx',
-            contents,
-            resolveDir: new URL('./', request.responseURL).pathname,
-          };
+          `;
 
-          await fileCache.setItem(args.path, res);
-          return res;
-        }
+        const res: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(args.path, res);
+        return res;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // else fetch and store response in cache
+        const { data, request } = await axios.get(args.path);
+        const res: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(args.path, res);
+        return res;
       });
     },
   };
