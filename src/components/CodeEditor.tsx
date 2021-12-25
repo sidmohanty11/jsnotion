826defc2e66f2d34f270d3d8511b2b0d927d8281
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-12-25 19:24:50 +0530

    editor config

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -7,6 +7,16 @@ const CodeEditor = () => {
       defaultLanguage="javascript"
       defaultValue="// write your code here!"
       theme="vs-dark"
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 20,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
     />
   );
 };
