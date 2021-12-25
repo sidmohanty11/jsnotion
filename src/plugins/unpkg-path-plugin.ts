bb6972e9f563e3539062ad44baf93c9b0ba5f44a
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-12-25 12:22:53 +0530

    separating resolve logics

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,45 +6,38 @@ const fileCache = localforage.createInstance({
   name: 'filecache',
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (userInput: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // if path is index.js(our entry point), return
+      build.onResolve({ filter: /(^index\.js$)/ }, async (args: any) => {
+        return { path: args.path, namespace: 'a' };
+      });
+
+      // if it is other than our index.js, fetch it from unpkg
+      build.onResolve({ filter: /^\.+\// }, async (args: any) => {
+        return {
+          path: new URL(
+            args.path,
+            // resolveDir here stores relative path to where the main file is located
+            'https://unpkg.com' + args.resolveDir + '/'
+          ).href,
+          namespace: 'a',
+        };
+      });
+
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-
-        // if path is index.js, return
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
-
-        // if it is other than our index.js, fetch it from unpkg
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            path: new URL(
-              args.path,
-              // resolveDir here stores relative path to where the main file is located
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-            namespace: 'a',
-          };
-        }
-
         // else if it is not a relative import, fetch the pkg specified
         return { path: `https://unpkg.com/${args.path}`, namespace: 'a' };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
         // our index.js
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
-            `,
+            contents: userInput,
           };
         } else {
           // fetch data from unkpg
