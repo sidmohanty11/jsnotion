b230f6fea9451e22d5bff5b73c0c59fedf381523
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-12-25 11:18:43 +0530

    bundling and resloving all path errors

--- src/index.tsx
+++ src/index.tsx
@@ -30,6 +30,7 @@ const App = () => {
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin()],
+      define: { 'process.env.NODE_ENV': '"production"', global: 'window' },
     });
 
     setCode(result.outputFiles[0].text);
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -8,36 +8,46 @@ export const unpkgPathPlugin = () => {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
 
+        // if path is index.js, return
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
         }
 
+        // if it is other than our index.js, fetch it from unpkg
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
-            path: new URL(args.path, args.importer + '/').href,
+            path: new URL(
+              args.path,
+              // resolveDir here stores relative path to where the main file is located
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
             namespace: 'a',
           };
         }
 
+        // else if it is not a relative import, fetch the pkg specified
         return { path: `https://unpkg.com/${args.path}`, namespace: 'a' };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
 
+        // our index.js
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         } else {
-          const { data } = await axios.get(args.path);
+          // fetch data from unkpg
+          const { data, request } = await axios.get(args.path);
           return {
             loader: 'jsx',
             contents: data,
+            resolveDir: new URL('./', request.responseURL).pathname,
           };
         }
       });
