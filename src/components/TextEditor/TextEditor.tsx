1a66ae96e1f29a115ab1b247934b9aa4e8320e68
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-12-26 13:44:20 +0530

    toggling of text editor

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
+  const editorRef = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        editorRef.current &&
+        event.target &&
+        editorRef.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={editorRef}>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={'# hi'} />
     </div>
   );
 };
