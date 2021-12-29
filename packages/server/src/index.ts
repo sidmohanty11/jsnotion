9181c2b3dad8171b52c38416af557fc689d7cecf
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-12-29 11:34:28 +0530

    setting up server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import path from "path";
+import { createCellsRouter } from "./routes/cell";
 
 export const serve = (
   port: number,
@@ -22,6 +23,8 @@ export const serve = (
     const pkgPath = require.resolve("client/build/index.html");
     app.use(express.static(path.dirname(pkgPath)));
   }
+
+  app.use(createCellsRouter(filename, dir));
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);
---
+++ packages/server/src/routes/cell.ts
@@ -0,0 +1,39 @@
+import express, { json } from "express";
+import fs from "fs/promises";
+import path from "path";
+
+interface Cell {
+  id: string;
+  content: string;
+  type: "text" | "code";
+}
+
+export const createCellsRouter = (filename: string, dir: string) => {
+  const fullPath = path.join(dir, filename);
+  const router = express.Router();
+
+  router.use(json());
+
+  router.get("/cells", async (req, res) => {
+    try {
+      const result = await fs.readFile(fullPath, { encoding: "utf-8" });
+      res.send(JSON.parse(result));
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        await fs.writeFile(fullPath, "[]", "utf-8");
+        res.send([]);
+      } else {
+        throw err;
+      }
+    }
+  });
+
+  router.post("/cells", async (req, res) => {
+    const { cells }: { cells: Cell[] } = req.body;
+    await fs.writeFile(fullPath, JSON.stringify(cells), "utf-8");
+
+    res.send({ status: "ok" });
+  });
+
+  return router;
+};
